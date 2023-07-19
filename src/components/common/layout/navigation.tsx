d60c18a218ef51119627703ef0c6766fbe9f7609
강선우 <74632731+NiNinanana@users.noreply.github.com>
2023-07-19 10:36:58 +0900

    yarn berry migration (#84)

--- src/components/common/layout/navigation.tsx
+++ src/components/common/layout/navigation.tsx
@@ -3,12 +3,11 @@ import React from "react";
 const Navigation = () => {
   // TODO: route에 값 넣어 버튼 클릭 시 그 페이지로 이동하도록 설정
   const navList = [
-    { route: "", title: "홀몸어르신 돌봄" },
-    { route: "", title: "무연고자 사랑의 장례식" },
+    { route: "", title: "나눔코리아 소개" },
+    { route: "", title: "봉사활동 프로그램" },
     { route: "", title: "봉사 참여하기" },
     { route: "", title: "후원하기" },
     { route: "", title: "게시판" },
-    { route: "", title: "나눔코리아 소개" },
     { route: "", title: "나눔코리아 파트너" },
   ];
 
